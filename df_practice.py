1f26f631de15b08ca335e4a37a14933dff51cce3
David Morales <demorales@email.arizona.edu>
2022-06-24 10:48:16 -0700

    removed the functions code
    Must call the functions using the 'from dfmgmt import *"

--- df_practice.py
+++ df_practice.py
@@ -57,60 +57,11 @@ data['year'] = data.index.year
 data['month'] = data.index.month
 
 # %%
-# Create a function to generate separate dfs based on key id
-def split_key_df(df, key):
-    """Creates a separate dataframe from a master list based on key id.
-    
-    A master dataframe that contains a key id column can be separated into subset
-    dfs based on the key id. This function is best utilized with a dictionary for
-    loop outlined below:
-
-    
-    Parameters
-    ----------
-    df : df
-        master dataframe from which to pull data
-    key : int
-        Key id for each climate station
-        
-    Returns
-    ----------
-    df[df['key'] == key]
-    """
-
-    return df[df['key'] == key]
-
 # Create a dictionary of dfs utilizing a for loop and the split_key_df function
 dfs = {key: split_key_df(data, key) for key in key_list}
 
-# %%
 # Create a function to generate separate dfs based on freq using a provided list of column names
 cols = ['precip', 'evap', 'tmax', 'tmin']
-
-def resample_mean(df, cols, freq):
-    """Resamples data from a df utilizing a preset list of column names.
-    
-    Combining a list of column names, a dataframe, and a frequency code,
-    this function resamples data according to the mean belonging to included 
-    column names at the specified intervals.
-
-    M = monthly; W = weekly; D = daily; Y = yearly
-
-    Parameters
-    ----------
-    df : df
-        dataframe that will have its data resampled
-    cols : list
-        list of column names that will have their data resampled
-    freq : str
-        Frequency code for establishing the interval of resampling
-
-    Returns
-    ---------- 
-    df[cols].resample(str(freq)).mean()
-    """
-
-    return df[cols].resample(str(freq)).mean()
 
 # Create a dictionary of dfs utilizing a for loop and the resample_mean
 dfs_m_resample = {key: resample_mean(dfs[key], cols, 'M') for key in key_list}
