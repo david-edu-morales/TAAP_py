30a652e0853c24e3b665b5444961d0100652eca6
David Morales <demorales@email.arizona.edu>
2022-07-07 14:10:07 -0500

    Automated 12-plot w/floop for US data

--- 22140_read-in.py
+++ 22140_read-in.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+from sklearn import linear_model
+from dfmgmt import *
 import seaborn as sns
 sns.set(rc={'figure.figsize':(11, 4)})
 
@@ -35,7 +37,7 @@ data = pd.concat(df_list)
 data = data[['date','precip','tmax','tmin','key']]
 
 # Swap strings to None type
-data = data.replace({-9999: None}, regex=True)
+data = data.replace({-999.0: None, -9999.0: None}, regex=True)
 
 # Set dates to correct format
 data['date'] = pd.to_datetime(data['date'], format='%Y%m%d').dt.strftime("%Y-%m-%d")
@@ -46,5 +48,75 @@ data = data.set_index('date')
 # Add month and year columns to df
 data['year'] = data.index.year
 data['month'] = data. index.month
+
+# %%
+# Create a dictionary of dfs utilizing a for loop and the split_key_df function
+dfs = {key: split_key_df(data, key) for key in key_list}
+
+# Create a function to generate separate dfs based on freq using a provided list of column names
+cols = ['precip', 'tmax', 'tmin']
+
+# Create a dictionary of dfs utilizing a for loop and the resample_mean
+dfs_mm = {key: resample_mean(dfs[key], cols, 'M') for key in key_list} # mm = monthly mean
+
+# Add month and year to each df in dict
+for key in key_list:
+       dfs_mm[key]['month'] = dfs_mm[key].index.month
+       dfs_mm[key]['year'] = dfs_mm[key].index.year
+
+# %%
+# Automate 12-plot monthly mean plot for variables
+# Set up data & variables
+start, end = 1976, 2016 # set time frame to last forty years
+month_str = ['Jan', 'Feb', 'Mar', 'Apr', 'May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+df_mm_lastforty = dfs_mm[22140].loc[str(start):str(end)]
+degree_sign = u'\N{DEGREE SIGN}'
+
+for col in cols:
+       fig = plt.figure(figsize=(24,16))
+       fig.subplots_adjust(hspace=0.2, wspace=0.2)
+       fig.suptitle("Monthly Mean for "+col+"\nClimate Station 22140", fontsize=30)
+       
+       for i in range(1,13):
+              ax = fig.add_subplot(3,4,i)
+              x = df_mm_lastforty[df_mm_lastforty.index.month == i].index.year
+              y = df_mm_lastforty[df_mm_lastforty.index.month == i][col]
+
+              ax.plot(x,y) # this plots the col values
+
+              # Col-alike subplot formatting              
+              ax.set_title(month_str[i-1], fontsize=20, fontweight='bold')
+
+              # Make the linear regression
+              database= df_mm_lastforty.loc[df_mm_lastforty['month']==i][[col,'year']]
+              database=database.dropna()
+
+              x_data = database['year'].values.reshape(database.shape[0],1)
+              y_data = database[col].values.reshape(database.shape[0],1)
+
+              reg = linear_model.LinearRegression().fit(x_data, y_data)
+              coef = reg.coef_
+              inter= reg.intercept_
+              y_estimate = coef*x_data+inter # y=mx+b, possible option to upgrade
+
+              ax.plot(x_data,y_estimate) # this plots the linear regression
+
+              # Col-dependent subplot formatting
+              if col == cols[0]:
+                     ax.set_ylabel('mm')
+                     ax.text(.1, .8,
+                             str(round((end-start)*coef[0,0],2))+'mm/40yr',
+                             transform=ax.transAxes,
+                             fontsize=24,
+                             color='red')
+              else:
+                     ax.set_ylabel(degree_sign+'C')
+                     ax.text(.1, .8,
+                             str(round((end-start)*coef[0,0],2))+degree_sign+'C/40yr',
+                             transform=ax.transAxes,
+                             fontsize=24,
+                             color='red')
+
+       plt.savefig('22140_'+col+'-mm')
 
 # %%
