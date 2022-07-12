db740df1558200f21ea6375877d8819cda87f676
David Morales <demorales@email.arizona.edu>
2022-07-12 12:52:53 -0500

    MC, video 3

--- monteCarlo_practice.py
+++ monteCarlo_practice.py
@@ -6,32 +6,42 @@ def rollDice():
     roll = random.randint(1, 100)
 
     if roll == 100:
-        print(roll, 'roll was 100, you lose. What are the odds? Play again!')
+        #print(roll, 'roll was 100, you lose. What are the odds? Play again!')
         return False
 
     elif roll <= 50:
-        print(roll, 'roll was 1-50, you lose. Play again!')
+        #print(roll, 'roll was 1-50, you lose. Play again!')
         return False
 
     elif 100 > roll > 50:
-        print(roll, 'roll was 51-99, you win! Play more!')
+        #print(roll, 'roll was 51-99, you win! Play more!')
         return True
 
 def simple_bettor(funds, initial_wager,wager_count):
-    value = fundswager = initial_wager
+    value = funds
+    wager = initial_wager
+
     currentWager = 0
 
     while currentWager < wager_count:
         if rollDice():
-            value += wager_count
+            value += wager
         else:
-            value -= wager_count
+            value -= wager
         
         currentWager += 1
-        print('Funds:', value)
 
-simple_bettor(10000, 100, 100)
+    if value < 0:
+        value = 'broke'
+    print('Funds:', value)
 
+x = 0
+
+while x < 100:
+    simple_bettor(10000, 100, 10000)
+    x += 1
+
+# %%
 x = 0
 
 while x < 100:
