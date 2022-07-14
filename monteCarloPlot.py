0b1c0971c97d6ac312989490898f1ac5673fcda6
David Morales <demorales@email.arizona.edu>
2022-07-14 13:25:00 -0500

    MC, vid 19

--- monteCarloPlot.py
+++ monteCarloPlot.py
@@ -2,8 +2,12 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import csv
+from mpl_toolkits.mplot3d import Axes3D
 
 # %%
+fig = plt.figure()
+ax = fig.add_subplot(111,projection='3d')
+
 def graph():
     with open('monteCarloLiberal.csv','r') as montecarlo: # 'r' signifies read
         datas = csv.reader(montecarlo, delimiter=',')
@@ -14,8 +18,12 @@ def graph():
             wagerCount = float(eachLine[2])
             pcolor = eachLine[3]
 
-            plt.scatter(wagerSizePercent, wagerCount, color=pcolor)
+            ax.scatter(wagerSizePercent, wagerCount, percentROI, color=pcolor)
 
+            ax.set_xlabel('Wager percent size')
+            ax.set_ylabel('Wager count')
+            ax.set_zlabel('Percent ROI')
+            
     plt.show()
 
 graph()
