a2298dc0c3a1a3e5dbc62230e6c8c7eb0a9f2a4c
David Morales <demorales@email.arizona.edu>
2022-07-21 12:46:33 -0500

    removed old code
    the original, list-oriented code was removed to clean up workspace

--- taap_main.py
+++ taap_main.py
@@ -85,6 +85,7 @@ for key in keylist_mx:
               for month in range(1,13):
                      ax = fig.add_subplot(3,4,month)    # creates a 12-plot fig (3r x 4c)
 
+                     # select data to plot
                      x = dict_mm_mx[key][dict_mm_mx[key].index.month == month].tail(40).index.year
                      y = dict_mm_mx[key][dict_mm_mx[key].index.month == month][col].tail(40)
 
@@ -111,7 +112,7 @@ for key in keylist_mx:
                      saveLine = '\n'+str(key)+','+str(col)+','+str(month)+','+str(40*coef[0,0])
 
                      saveFile = open(csvFile, 'a')   # reopen csv file
-                     saveFile.write(saveLine)                                # append the saved row
+                     saveFile.write(saveLine)        # append the saved row
                      saveFile.close()
 
                      # Col-dependent subplot formatting
@@ -137,128 +138,12 @@ for key in keylist_mx:
                                    fontsize=24,
                                    color='red')
 
+                     #plt.savefig(str(key)+'_'+col+'_'+month_str[month-1]+'-mm')
+
 # %%
 # Return recorded coefficients into dictionary
 dfCoef = pd.read_csv(csvFile, delimiter=',', usecols=headerList)
 dictCoef = {key: dfCoef[dfCoef['key'] == key] for key in keylist_mx}
-
-# %%
-key_list_mx = [26013, 26057, 26164]
-filename_list_mx = [str(key_list_mx[key])+'_daily-record.txt' for key in range(len(key_list_mx))]
-# Create a list of dataframes
-df_list_mx = [pd.read_fwf(filename,
-                          skiprows=19,
-                          skipfooter=1,
-                          names=['date',
-                                 'precip',
-                                 'evap',
-                                 'tmax',
-                                 'tmin'])
-              for filename in filename_list_mx]
-
-# Add climate station key for each df
-for k in range(len(key_list_mx)):
-       df_list_mx[k]['key'] = key_list_mx[k]
-
-# Concatenate dataframes from list into a single dataframe
-data_mx = pd.concat(df_list_mx)
-
-# Swap strings to None type
-data_mx = data_mx.replace({'Nulo': None}, regex=True)
-data_mx = data_mx.replace({'ul' : None}, regex=True)    # Necessary, but could not find anything using 
-                                                        # data.loc[data['evap'] == 'ul']
-
-# Set dates to correct format
-data_mx['date'] = pd.to_datetime(data_mx['date'],
-                                 infer_datetime_format=True,
-                                 dayfirst=True,
-                                 format='%Y-%m-%d')
-
-# Set index to datetimeIndex
-data_mx = data_mx.set_index('date')
-
-# Assign float type to data and int type to key
-data_mx = data_mx.astype(float)
-data_mx['key'] = data_mx['key'].astype(int)      # reset key to integer
-
-# Add month and year columns to df
-data_mx['year'] = data_mx.index.year
-data_mx['month'] = data_mx.index.month
-
-# Create a dictionary of dfs utilizing a for loop and the split_key_df function
-dfs_mx = {key: split_key_df(data_mx, key) for key in key_list_mx}
-
-# Create a function to generate separate dfs based on freq using a provided list of column names
-cols_mx = ['precip', 'evap', 'tmax', 'tmin']
-
-# Create a dictionary of dfs utilizing a for loop and the resample_mean
-dfs_mm_mx = {key: resample_mean(dfs_mx[key], cols_mx, 'M') for key in key_list_mx}
-
-# Add month and year
-dfs_mm_mx[26057]['year']=dfs_mm_mx[26057].index.year
-dfs_mm_mx[26057]['month']=dfs_mm_mx[26057].index.month
-
-# %%
-# Automate 12-plot monthly mean plot for variables
-# Set up data & variables
-start, end = 1976, 2016 # set time frame to last forty years
-month_str = ['Jan', 'Feb', 'Mar', 'Apr', 'May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-df_mm_mx_last40 = dfs_mm_mx[26057].loc[str(start):str(end)]
-degree_sign = u'\N{DEGREE SIGN}'
-
-for col in cols_mx:
-       fig = plt.figure(figsize=(24,16))
-       fig.subplots_adjust(hspace=0.2, wspace=0.2)
-       fig.suptitle("Monthly Mean for "+col+"\nClimate Station 26057", fontsize=30)
-       
-       for i in range(1,13):
-              ax = fig.add_subplot(3,4,i)
-              x = df_mm_mx_last40[df_mm_mx_last40.index.month == i].index.year
-              y = df_mm_mx_last40[df_mm_mx_last40.index.month == i][col]
-
-              ax.plot(x,y) # this plots the col values
-
-              # Col-alike subplot formatting              
-              ax.set_title(month_str[i-1], fontsize=20, fontweight='bold')
-
-              # Make the linear regression
-              database= df_mm_mx_last40.loc[df_mm_mx_last40['month']==i][[col,'year']]
-              database=database.dropna()
-
-              x_data = database['year'].values.reshape(database.shape[0],1)
-              y_data = database[col].values.reshape(database.shape[0],1)
-
-              reg = linear_model.LinearRegression().fit(x_data, y_data)
-              coef = reg.coef_
-              inter= reg.intercept_
-              y_estimate = coef*x_data+inter # y=mx+b, possible option to upgrade
-
-              ax.plot(x_data,y_estimate) # this plots the linear regression
-
-              # Col-dependent subplot formatting
-              if col == cols_mx[0]:
-                     ax.set_ylabel('mm')
-                     ax.text(.1, .8,
-                             str(round((end-start)*coef[0,0],2))+'mm/40yr',
-                             transform=ax.transAxes,
-                             fontsize=24,
-                             color='red')
-              elif col == cols_mx[1]: # cannot figure out how to combine cols_mx[0:2]
-                     ax.set_ylabel('mm')
-                     ax.text(.1, .8,
-                             str(round((end-start)*coef[0,0],2))+'mm/40yr',
-                             transform=ax.transAxes,
-                             fontsize=24,
-                             color='red')
-              else:
-                     ax.set_ylabel(degree_sign+'C')
-                     ax.text(.1, .8,
-                             str(round((end-start)*coef[0,0],2))+degree_sign+'C/40yr',
-                             transform=ax.transAxes,
-                             fontsize=24,
-                             color='red')
-
-       plt.savefig('26057_'+col+'-mm')
 
 # %%
 # *** US CLIMATE STATIONS ***
@@ -377,24 +262,6 @@ for col in cols_us:
 day_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # "normal" year distro
 ly_list = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # leap-year distro
 
-for i in range(12):
-       dfs_mm_mx[26057]['precip_cum'] = dfs_mm_mx[26057][dfs_mm_mx[26057]['month'] == i+1][['precip']] * day_list[i]
-       
-# %%
-# Create a dictionary of 
-dict_cmm_mx = {month: dfs_mm_mx[26057][dfs_mm_mx[26057]['month'] == month+1][['precip']] * day_list[month] for month in range(12)}
-df_cmm_26057 = pd.concat(dict_cmm_mx.values(), axis=0)
-df_cmm_26057 = df_cmm_26057.sort_index()
-df_cmm_26057 = df_cmm_26057.rename(columns={'precip':'precip_cum'})
-
-# %%
-# Brief stat analysis
-for m in range(1,13):
-       print(df_cmm_26057[df_cmm_26057.index.month == m].std())
-
-# %%
-dict_tmax_cmm_mx = {month: dfs_mm_mx[26057][dfs_mm_mx[26057]['month'] == month+1][['tmax']] for month in range(12)}
-
 # %%
 # Monte Carlo function to generate randomized sample of observed values for given timeframe.
 def monteCarloGenerator(obsValueList):
@@ -420,108 +287,10 @@ def monteCarloGenerator(obsValueList):
        selValDf = selValDf.dropna()              # drop NaN-bearing rows from dataframe
 
        x_data = selValDf['year'].values.reshape(selValDf.shape[0],1)   # prep data for linreg
-       y_data = selValDf['precip'].values.reshape(selValDf.shape[0],1)
+       y_data = selValDf['variable'].values.reshape(selValDf.shape[0],1)
 
        reg = linear_model.LinearRegression().fit(x_data, y_data)
-       coef = reg.coef_
-
-# %%
-# Monte Carlo simulator to evaluate the significance of observed changes in mensual tmax.
-def monteCarloTmax(tmaxList):
-       global coef   # linear regression variable will be added to list in iterator
-
-       tX = [] # list to collect count of years for x-axis
-       vY = [] # list to collect selected precip values for y-axis
-
-       currentYear = 1      # counter to keep track of years
-
-       while currentYear <= 40:
-              roll = randint(0,39)        # generate random index place value
-              tmaxValue = tmaxList[roll]         # select corresponding precip value from list
-              tX.append(currentYear)             # add year value to list
-              vY.append(tmaxValue)               # add precip value to list
-
-              currentYear += 1
-              
-       # Calculate the linear regression
-       linearRegDict = {'year':tX, 'tmax':vY}  # merge year and precip lists into dictionary
-       linearRegDf = pd.DataFrame(linearRegDict) # convert dictionary into dataframe
-       linearRegDf = linearRegDf.dropna()        # drop nan-bearing rows from dataframe
-
-       x_data = linearRegDf['year'].values.reshape(linearRegDf.shape[0],1)   # prep data for linreg
-       y_data = linearRegDf['tmax'].values.reshape(linearRegDf.shape[0],1)
-
-       reg = linear_model.LinearRegression().fit(x_data, y_data)
-       coef = reg.coef_
-
-# %%
-# get start datetime
-startTime = datetime.now()
-
-# Set code for iterator
-janPrecipCum = dict_cmm_mx[0]['precip'].tail(40).values.tolist()      # example of target list for while loop
-#dict_cmm_mx[0]['precip'].tail(40).plot()                              # actual plot of target list values
-
-# set variables for iterator
-sampSize = 10000 # number of iterations for Monte Carlo simulator
-counter = 1   # counter to keep track of iterated distributions
-linRegCoef = [] # create list for store linreg coefficients
-
-# iterate monte carlo simulator code
-while counter <= sampSize:  # setting the number of iterations to the chosen sample size
-       monteCarloGenerator(janPrecipCum)
-       linRegCoef.append(40*coef[0,0])
-       #plt.show()    # creates separate graphs for each iteration, comment out for one main plot
-       
-       counter += 1
-
-# plot distribution of coefficients onto histogram
-coefSeries = pd.Series(linRegCoef) # convert list of linreg coefficients to series
-ax = coefSeries.plot.hist(bins=50)
-ax.set_xlabel('[mm/40yr]')    
-ax.set_title('Monte Carlo Analysis of January Precipitation\nClimate Station 26057')
-
-# get end datetime
-endTime = datetime.now()
-
-# get execution time
-elapsedTime = endTime - startTime
-print('Execution time:', elapsedTime)
-
-# %%
-# get start datetime
-startTime = datetime.now()
-
-# Set code for iterator
-marTmax = dict_tmax_cmm_mx[2]['tmax'].tail(40).values.tolist()        # example of target list for while loop
-#dict_tmax_cmm_mx[2]['tmax'].tail(40).plot()                          # sample of actual plot
-
-# set variables for iterator
-sampSize = 100000 # number of iterations for Monte Carlo simulator
-counter = 1   # counter to keep track of iterated distributions
-linRegCoef = [] # create list for store linreg coefficients
-
-# iterate monte carlo simulator code
-while counter <= sampSize:  # setting the number of iterations to the chosen sample size
-       monteCarloPrecip(marTmax)
-       linRegCoef.append(40*coef[0,0])
-       #plt.show()    # creates separate graphs for each iteration, comment out for one main plot
-       
-       counter += 1
-
-# plot distribution of coefficients onto histogram
-coefSeries = pd.Series(linRegCoef) # convert list of linreg coefficients to series
-ax = coefSeries.plot.hist(bins=50)
-ax.set_xlabel(degree_sign+'C/40yr')    
-ax.set_title('Monte Carlo Analysis of March tmax\nClimate Station 26057, n=' + str(sampSize))
-# ax.axvline(5.24, color='r') # shows the corresponding linreg coefficient value for 26057/tmax/March
-
-# get end datetime
-endTime = datetime.now()
-
-# get execution time
-elapsedTime = endTime - startTime
-print('Execution time:', elapsedTime)
+       coef = reg.coef_     # define linreg trend as coef to be plot and saved in iterator
 
 # %%
 # Automate the iterator to run through all station/variable/month MCA distributions
@@ -541,7 +310,7 @@ for key in keylist_mx:
                      obsCoef = dictCoef[key][(dictCoef[key]['variable']==col)\
                                & (dictCoef[key]['month']==month)]['coef'].values[0]
 
-                     sampSize = 10000     # number of iterations for MCA
+                     sampSize = 10     # number of iterations for MCA
                      counter = 1          # counter to keep track of iterated distributions
                      linRegCoef = []      # create list for storing generated linreg coefs
 
