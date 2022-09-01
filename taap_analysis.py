d1218b85fe1a54e1de583a950c3b2fabdb41711d
David Morales <demorales@email.arizona.edu>
2022-08-31 23:36:55 -0700

    cleaned the analysis

--- taap_analysis.py
+++ taap_analysis.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 from matplotlib.dates import MonthLocator, DateFormatter
+import datetime as dt
 
 # %%
 # Set the station IDs
@@ -35,31 +36,49 @@ for var in varsAvg_mx:
 
 # %%
 fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(12, 6.5), sharex=True, sharey=True)
-plt.subplots_adjust(hspace=0.15)
+plt.subplots_adjust(hspace=0.2)
 plt.suptitle('Percent chance Monte Carlo-generated trend\nis greater than historical trend')
 fig.text(0.08, 0.5, '% chance', va='center', rotation='vertical', fontsize=16)
 plt.xlabel('Month', fontsize=15)
-plt.xlim([.9,12.1])
 
 for n, var in enumerate(varsAvg_mx):
     ax = plt.subplot(3, 1, n+1)
 
     for key in keylist_mx:
-        mask = (dictCoefAvg[key].variable == var)
-        df = dictCoefAvg[key].loc[mask].set_index('month')
+        # Select subset of dataframe for plotting
+        mask = (dictCoefAvg[key].variable == var)           # bool mask set to looped variable
+        df = dictCoefAvg[key].loc[mask].set_index('month')  # dataset from bool index and reset index
+        df.index = pd.to_datetime(df.index, format='%m')    # convert index to datetimeindex
+
         x = df.index
         y = df.chance
-        plt.title(var.capitalize(), fontsize=12, fontweight='bold')
-        plt.plot(x,y, marker='o', fillstyle='full', markerfacecolor='white', label='Station '+str(key))
-        plt.xticks(np.arange(min(x), max(x)+1, 1.0))
-        plt.yticks(np.arange(0, 50, 10.0))
-        plt.hlines(5, xmin=0, xmax=12.1, colors='k', linestyles='dashed', alpha=0.25)
+
+        plt.title(var.capitalize(),             # set subplot title
+                  fontsize=12,
+                  fontweight='bold')
+        plt.plot(x, y,                          # plot data
+                 marker='o',
+                 fillstyle='full',
+                 markerfacecolor='white',
+                 label='Station '+str(key))
+        plt.hlines(5, xmin=dt.date(1900,1,1),   # plot 5% threshold for significant data
+                      xmax=dt.date(1900,12,31),
+                      colors='k',
+                      linestyles='dashed',
+                      alpha=0.25)
+        plt.yticks(np.arange(0, 50, 10.0))      # edit y-ticks for clarity
+
+        ax.set_xlim([dt.date(1899, 12, 31), dt.date(1900, 12, 2)])  # set x-axis limit
+
+        # Rename datetime x-ticks to the first letter of each month
+        month_fmt = DateFormatter('%b')
+        def m_fmt(x, pos=None):
+            return month_fmt(x)[0]
+        ax.xaxis.set_major_locator(MonthLocator())
+        ax.xaxis.set_major_formatter(FuncFormatter(m_fmt))
 
 handles, labels = ax.get_legend_handles_labels()
 fig.legend(handles, labels, bbox_to_anchor=(.2625,.88))
 
 plt.savefig('graphs/prctChance-avg')
 plt.show()
-
-
-    # %%
